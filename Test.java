9999706f51884a0e496cb81fe5c3f73599d77ca2
JavicxhloWong <752097910@qq.com>
2022-02-22 14:23:24 +0800

    quick sort

--- Test.java
+++ Test.java
@@ -4,8 +4,9 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] testArr = {6, 3, 8, 2, 9, 1};
-		int[] res = BubbleSort.bubbleSort(testArr);
+		QuickSort quickSort = new QuickSort();
+		int[] testArr = {0, 6, 3, 8, 2, 0, 9, 1};
+		int[] res = quickSort.quickSort(testArr);
 		for(int i: res) {
 			System.out.println(i);
 		}
